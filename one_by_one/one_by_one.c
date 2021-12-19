916ac9cd4d1ab1a757064490151388f5e6645bf9
Nidhi Agarwal <agarwalnidhi4321@gmail.com>
2021-12-19 15:01:20 +0530

    First commit to github

---
+++ one_by_one/one_by_one.c
@@ -0,0 +1,24 @@
+#include<avr/io.h>
+#include<util/delay.h>
+int main()
+{
+DDRC=0b11111111;
+while(1)
+{
+PORTC=0b00000001;
+_delay_ms(200);
+PORTC=0b00000010;
+_delay_ms(200);
+PORTC=0b00000100;
+_delay_ms(200);
+PORTC=0b00001000;
+_delay_ms(200);
+PORTC=0b00010000;
+_delay_ms(200);
+PORTC=0b00100000;
+_delay_ms(200);
+PORTC=0b01000000;
+_delay_ms(200);
+PORTC=0b10000000;
+}
+}
